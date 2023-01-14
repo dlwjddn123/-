b28df5a407d204a3bb4c8eb8f0c54161035d5252
이정우 <acg6138@naver.com>
2023-01-14 10:38:25 +0900

    [FEAT] : Jwt를 생성하고 검증하는 JwtTokenUtil 구현, Jwt헤더와 만료 기간에 대한 상수 정의, 엔트리 포인트 구현

---
+++ src/main/java/com/footstep/global/config/jwt/constants/JwtExpiration.java
@@ -0,0 +1,15 @@
+package com.footstep.global.config.jwt.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public enum JwtExpiration {
+
+    ACCESS_TOKEN_EXPIRATION_TIME("JWT 만료 시간 / 7일", 1000L * 60 * 30 * 24 * 7),
+    REFRESH_TOKEN_EXPIRATION_TIME("Refresh 토큰 만료 시간 / 30일", 1000L * 60 * 60 * 24 * 30),
+    REISSUE_EXPIRATION_TIME("Refresh 토큰 만료 시간 / 30일", 1000L * 60 * 60 * 24 * 3);
+
+    private String description;
+    private Long value;
+}
