ca84470cc77486114d02b98ce7e31110c6c50e97
이정우 <acg6138@naver.com>
2023-01-14 10:40:59 +0900

    [FIX] : REISSUE_EXPIRATION_TIME 값의 오류 수정

--- src/main/java/com/footstep/global/config/jwt/constants/JwtExpiration.java
+++ src/main/java/com/footstep/global/config/jwt/constants/JwtExpiration.java
@@ -8,7 +8,7 @@ public enum JwtExpiration {
 
     ACCESS_TOKEN_EXPIRATION_TIME("JWT 만료 시간 / 7일", 1000L * 60 * 30 * 24 * 7),
     REFRESH_TOKEN_EXPIRATION_TIME("Refresh 토큰 만료 시간 / 30일", 1000L * 60 * 60 * 24 * 30),
-    REISSUE_EXPIRATION_TIME("Refresh 토큰 만료 시간 / 30일", 1000L * 60 * 60 * 24 * 3);
+    REISSUE_EXPIRATION_TIME("Refresh 토큰 만료 시간 / 30일", 1000L * 60 * 60 * 24 * 30);
 
     private String description;
     private Long value;
