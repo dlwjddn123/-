acaf4d541cf8501b51eb3d720bdb0e2eefd5bcb7
이정우 <acg6138@naver.com>
2023-01-15 16:22:54 +0900

    [FEAT] : 마이 페이지, 프로필 정보 변경, 회원 탈퇴 로직 구현

--- src/main/java/com/footstep/domain/users/controller/UsersController.java
+++ src/main/java/com/footstep/domain/users/controller/UsersController.java
@@ -1,11 +1,52 @@
 package com.footstep.domain.users.controller;
 
+import com.footstep.domain.users.domain.Users;
+import com.footstep.domain.users.dto.ChangePasswordInfo;
+import com.footstep.domain.users.dto.MyPageInfo;
+import com.footstep.domain.users.dto.TokenDto;
+import com.footstep.domain.users.repository.UsersRepository;
+import com.footstep.domain.users.service.UsersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
 @RequiredArgsConstructor
 public class UsersController {
+
+    private final UsersService usersService;
+    private final UsersRepository usersRepository;
+
+    @GetMapping("/my-page")
+    public MyPageInfo getMyPage(Authentication authentication) {
+        return usersService.getMyPage(authentication);
+    }
+
+    @PatchMapping("/my-page/password")
+    public void changePassword(Authentication authentication, @RequestBody ChangePasswordInfo changePasswordInfo) {
+        usersService.changePassword(authentication, changePasswordInfo);
+    }
+
+
+    @PatchMapping("/my-page/nickname")
+    public void changeNickname (Authentication authentication, @RequestBody Map<String, String> nicknameMap) {
+        usersService.changeNickname(authentication, nicknameMap.get("nickname"));
+    }
+
+    @PatchMapping("/my-page/profile")
+    public void changeProfileImage(Authentication authentication, @RequestBody Map<String, String> imageUrlMap) {
+        usersService.changeProfileImage(authentication, imageUrlMap.get("imageUrl"));
+    }
+
+    @PatchMapping("/my-page/secession")
+    public void secession(@RequestHeader("Authorization") String accessToken,
+                          @RequestHeader("RefreshToken") String refreshToken,
+                          Authentication authentication) {
+        usersService.secession(authentication, TokenDto.of(accessToken, refreshToken));
+    }
 }
 
--- src/main/java/com/footstep/domain/users/service/UsersService.java
+++ src/main/java/com/footstep/domain/users/service/UsersService.java
@@ -1,13 +1,61 @@
 package com.footstep.domain.users.service;
 
+import com.footstep.domain.users.domain.Users;
+import com.footstep.domain.users.dto.ChangePasswordInfo;
+import com.footstep.domain.users.dto.MyPageInfo;
+import com.footstep.domain.users.dto.TokenDto;
+import com.footstep.domain.users.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class UsersService {
+
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
+    
+    public MyPageInfo getMyPage(Authentication authentication) {
+        UserDetails details = (UserDetails) authentication.getPrincipal();
+        Users currentUsers = usersRepository.findByEmail(details.getUsername()).orElseThrow(() -> new NullPointerException("로그인이 필요합니다."));
+        return new MyPageInfo(currentUsers.getNickname(), currentUsers.getPostings().size());
+    }
+
+    public void changePassword(Authentication authentication, ChangePasswordInfo changePasswordInfo) {
+        UserDetails details = (UserDetails) authentication.getPrincipal();
+        Users users = usersRepository.findByEmail(details.getUsername()).orElseThrow(() -> new NullPointerException("로그인이 필요합니다."));
+        if (!passwordEncoder.matches(changePasswordInfo.getCurrentPassword(), users.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        users.changePassword(passwordEncoder.encode(changePasswordInfo.getChangedPassword()));
+        usersRepository.save(users);
+    }
+
+    public void changeNickname(Authentication authentication, String nickname) {
+        UserDetails details = (UserDetails) authentication.getPrincipal();
+        Users users = usersRepository.findByEmail(details.getUsername()).orElseThrow(() -> new NullPointerException("로그인이 필요합니다."));
+        users.changeNickname(nickname);
+        usersRepository.save(users);
+    }
+
+    public void changeProfileImage(Authentication authentication, String imageUrl) {
+        UserDetails details = (UserDetails) authentication.getPrincipal();
+        Users users = usersRepository.findByEmail(details.getUsername()).orElseThrow(() -> new NullPointerException("로그인이 필요합니다."));
+        users.changeProfileImage(imageUrl);
+        usersRepository.save(users);
+    }
+
+    public void secession(Authentication authentication, TokenDto tokenDto) {
+        UserDetails details = (UserDetails) authentication.getPrincipal();
+        Users users = usersRepository.findByEmail(details.getUsername()).orElseThrow(() -> new NullPointerException("로그인이 필요합니다."));
+        users.secession();
+        authService.logout(tokenDto, users.getEmail());
+        usersRepository.save(users);
+    }
 }
