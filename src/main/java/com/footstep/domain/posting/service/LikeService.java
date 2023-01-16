beb7aa77a3298c0d65762815e035a5993bc7284e
DAHLIACHOI <cyj000424@naver.com>
2023-01-17 01:24:30 +0900

    [FEAT] : 좋아요 기능 구현

---
+++ src/main/java/com/footstep/domain/posting/service/LikeService.java
@@ -0,0 +1,54 @@
+package com.footstep.domain.posting.service;
+
+
+import com.footstep.domain.posting.domain.Likes;
+import com.footstep.domain.posting.domain.posting.Posting;
+import com.footstep.domain.posting.repository.LikeRepository;
+import com.footstep.domain.posting.repository.PostingRepository;
+import com.footstep.domain.users.domain.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class LikeService {
+    private final LikeRepository likeRepository;
+    private final PostingRepository postingRepository;
+
+    public boolean addLike(Users users, Long postingId) {
+        Posting posting = postingRepository.findById(postingId).orElseThrow();
+        if (isNotLiked(users, posting)) {
+            likeRepository.save(new Likes(users, posting));
+            return true;
+        }
+        return false;
+    }
+
+    public void cancelLike(Users users, Long postingId) {
+        Posting posting = postingRepository.findById(postingId).orElseThrow();
+        Likes likes = likeRepository.findByUsersAndPosting(users, posting).orElseThrow();
+        likeRepository.delete(likes);
+    }
+
+    public List<String> count(Long postingId, Users targetUser) {
+        Posting posting = postingRepository.findById(postingId).orElseThrow();
+        Integer likeCount = likeRepository.countByPosting(posting).orElse(0);
+        List<String> result = new ArrayList<>(Arrays.asList(String.valueOf(likeCount)));
+        if(Objects.nonNull(targetUser)){
+            result.add(String.valueOf(isNotLiked(targetUser, posting)));
+            return result;
+        }
+        return result;
+    }
+
+    private boolean isNotLiked(Users users, Posting posting) {
+        return likeRepository.findByUsersAndPosting(users, posting).isEmpty();
+    }
+}
