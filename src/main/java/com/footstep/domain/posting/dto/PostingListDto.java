7b05c21b0b0286c9008b9a5e0e3b0ff7833d3a4a
이정우 <acg6138@naver.com>
2023-01-18 19:35:37 +0900

    [FEAT] : 게시물에 대한 정보와 게시글 날짜별 카테고리 개수를 반환하는 Dto 생성

---
+++ src/main/java/com/footstep/domain/posting/dto/PostingListDto.java
@@ -0,0 +1,23 @@
+package com.footstep.domain.posting.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostingListDto {
+
+    private String placeName;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date recordDate;
+    private String imageUrl;
+    private String title;
+    private Long likes;
+    private Long postings;
+    private Long postingId;
+}
