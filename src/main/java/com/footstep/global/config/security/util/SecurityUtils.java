e2edb76667d76040ee4f22ea0c87728d391ad00a
길지운 <wldns2577@khu.ac.kr>
2023-01-20 03:23:42 +0900

    [UPDATE] : 사용자 인증 로직 수정

--- src/main/java/com/footstep/global/config/security/util/SecurityUtils.java
+++ src/main/java/com/footstep/global/config/security/util/SecurityUtils.java
@@ -1,8 +1,10 @@
 package com.footstep.global.config.security.util;
 
+import com.footstep.domain.base.BaseException;
 import com.footstep.domain.users.domain.Users;
 import com.footstep.domain.users.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -12,7 +14,7 @@ public class SecurityUtils {
     private final UsersRepository usersRepository;
 
     public static String getLoggedUserEmail() {
-        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return principal.getUsername();
+        Authentication principal = SecurityContextHolder.getContext().getAuthentication();
+        return principal.getName();
     }
 }
