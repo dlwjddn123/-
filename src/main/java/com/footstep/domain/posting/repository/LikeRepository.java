215fbad4ecbe1893b6b34e779dc0ca10d7d0d205
DAHLIACHOI <cyj000424@naver.com>
2023-01-29 17:36:15 +0900

    [FIX] : 좋아요 기능 로직 수정

--- src/main/java/com/footstep/domain/posting/repository/LikeRepository.java
+++ src/main/java/com/footstep/domain/posting/repository/LikeRepository.java
@@ -4,6 +4,8 @@ import com.footstep.domain.posting.domain.Likes;
 import com.footstep.domain.posting.domain.posting.Posting;
 import com.footstep.domain.users.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +13,7 @@ public interface LikeRepository extends JpaRepository<Likes, Long> {
     Optional<Integer> countByPosting(Posting posting);
 
     Optional<Likes> findByUsers(Users users);
+
+    @Query("SELECT l FROM Likes l WHERE l.users = :users AND l.posting = :posting")
+    Optional<Likes> findByUsersAndPosting(@Param("users") Users users, @Param("posting") Posting posting);
 }
