aa8b72b4e411bc50a030f3692f609ee57de15c15
길지운 <wldns2577@khu.ac.kr>
2023-02-05 00:06:21 +0900

    [COMMENT] : 주석 오타 수정

--- src/main/java/com/footstep/domain/base/BaseException.java
+++ src/main/java/com/footstep/domain/base/BaseException.java
@@ -3,10 +3,11 @@ package com.footstep.domain.base;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class BaseException extends Exception {
-    private BaseResponseStatus status;  //BaseResoinseStatus 객체에 매핑
+    private BaseResponseStatus status;  //BaseResponseStatus 객체에 매핑
 }
